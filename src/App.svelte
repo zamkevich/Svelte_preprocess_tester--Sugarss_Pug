fba439647d566f7c4ca633d9f1b6e2ef7bcf7a8f
Misha Zamkevich <uwwcom@gmail.com>
2020-04-06 04:21:19 +1000

    Issue 1.1 is ready

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,20 @@
 <script>
 	export let name;
+	
+	function onInput() {
+	  console.log('foo')
+	}
 </script>
 
 <template lang="pug">
 	main
-		h1 Hello {name}
+		h1 Hello {name} 1212
 		p Visit the 
 			a(href="https://svelte.dev/tutorial") Svelte tutorial 
 			| to learn how to build Svelte apps.
 
+		input(bind:value="{name}")
+		input(on:input="{onInput}")
 </template>
 
 <style lang="postcss">
